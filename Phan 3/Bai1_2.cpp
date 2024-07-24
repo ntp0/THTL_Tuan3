19b4b3a684087dd31ca7d6b905f595a479bcff2c
Nguyen Thanh phong <ntp012352gmail.com>
2024-07-24 14:05:44 +0700

    Add Bai1_2

---
+++ Phan 3/Bai1_2.cpp
@@ -0,0 +1,64 @@
+#include <conio.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <iostream>
+
+void createRandomMatrix(int** a, int m, int n, int k) {
+	for (int i = 0; i < m; i++) {
+		for (int j = 0; j < n; j++) {
+			a[i][j] = rand() % (k + 1);
+		}
+	}
+}
+
+void printMatrix(int** a, int m, int n) {
+	for (int i = 0; i < m; i++) {
+		for (int j = 0; j < n; j++) {
+			printf("%4d", a[i][j]);
+		}
+		printf("\n");
+	}
+}
+
+void printRowSums(int** a, int m, int n) {
+	for (int i = 0; i < m; i++) {
+		int sum = 0;
+		for (int j = 0; j < n; j++) {
+			sum += a[i][j];
+		}
+		printf("Tong dong %d: %d\n", i, sum);
+	}
+}
+
+int main() {
+	int m, n, k;
+	srand(time(0));
+
+	printf("Nhap so dong cua ma tran (m): ");
+	scanf("%d", &m);
+	printf("Nhap so cot cua ma tran (n): ");
+	scanf("%d", &n);
+	printf("Nhap gia tri lon nhat cua cac phan tu (k): ");
+	scanf("%d", &k);
+
+	int** a = (int**)malloc(m * sizeof(int*));
+	for (int i = 0; i < m; i++) {
+		a[i] = (int*)malloc(n * sizeof(int));
+	}
+
+	createRandomMatrix(a, m, n, k);
+	printf("Ma tran ngau nhien:\n");
+	printMatrix(a, m, n);
+
+	printf("Tong gia tri cua tung dong:\n");
+	printRowSums(a, m, n);
+
+
+	for (int i = 0; i < m; i++) {
+		free(a[i]);
+	}
+	free(a);
+	system("pause");
+	return 0;
+}
